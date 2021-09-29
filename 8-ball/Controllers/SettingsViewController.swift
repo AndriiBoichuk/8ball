30c2ebce3f376a5e8a5ece652042780598203d53
Andrii <andriyboychuk0110@gmail.com>
2021-09-29 11:38:37 +0300

    Completed design for SettingsView, added logic to this view

--- 8-ball/Controllers/SettingsViewController.swift
+++ 8-ball/Controllers/SettingsViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    
+    @IBOutlet weak var answerTextField: UITextField!
+    @IBOutlet weak var saveButtonView: UIView!
+    @IBOutlet weak var saveButton: UIButton!
+    
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
@@ -22,12 +28,32 @@ class SettingsViewController: UIViewController {
         navigationController?.navigationBar.isTranslucent = true
         navigationController?.view.backgroundColor = .clear
         
+        saveButtonView.layer.cornerRadius = 10
+        saveButtonView.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
         
+        answerTextField.layer.cornerRadius = 10
+        answerTextField.layer.sublayerTransform = CATransform3DMakeTranslation(5, 0, 0);
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+        if answerTextField.text == "" {
+            saveButton.isEnabled = false
+        } else {
+            saveButton.isEnabled = true
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        saveButton.isEnabled = false
     }
+    
+    
+    @IBAction func saveTouched(_ sender: UIButton) {
+        answerTextField.text = ""
+        print("Save touched")
+    }
+    
 }
